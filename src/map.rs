59c2a0d3a7abb00904ea3142369fcec6fa2f59a9
Dylan Adam <dylan41ad@hotmail.fr>
2025-06-25 22:32:48 +0200

    add water animation

--- src/map.rs
+++ src/map.rs
@@ -1,14 +1,24 @@
 use bevy::prelude::*;
 
-const TILE_SIZE: f32 = 16.0;
+const TILE_SIZE: u32 = 16;
 const TILE_SCALE: f32 = 3.0;
-const SCALED_TILE_SIZE: f32 = TILE_SIZE * TILE_SCALE;
+const SCALED_TILE_SIZE: f32 = TILE_SIZE as f32 * TILE_SCALE;
+
+#[derive(Component, Clone, Copy)]
+struct WaterAnimationIndices {
+    first: usize,
+    last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct WaterAnimationTimer(Timer);
 
 pub struct MapPlugin;
 
 impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, load_map);
+        app.add_systems(Startup, load_map)
+            .add_systems(Update, water_animation);
     }
 }
 
@@ -19,9 +29,11 @@ fn load_map(
 ) {
     // spawn water layer
     let water_tileset = asset_server.load("tilesets/Water.png");
-    let water_layout = TextureAtlasLayout::from_grid(UVec2::splat(16), 4, 1, None, None);
+    let water_layout = TextureAtlasLayout::from_grid(UVec2::splat(TILE_SIZE), 4, 1, None, None);
 
     let water_atlas_layout = texture_atlas_layouts.add(water_layout);
+
+    let water_animation_indices = WaterAnimationIndices { first: 0, last: 3 };
 
     for x in 0..60 {
         for y in 0..40 {
@@ -30,7 +42,7 @@ fn load_map(
                     water_tileset.clone(),
                     TextureAtlas {
                         layout: water_atlas_layout.clone(),
-                        index: 0,
+                        index: water_animation_indices.first,
                     },
                 ),
                 Transform {
@@ -42,7 +54,32 @@ fn load_map(
                     scale: Vec3::splat(TILE_SCALE),
                     ..default()
                 },
+                water_animation_indices,
+                WaterAnimationTimer(Timer::from_seconds(0.3, TimerMode::Repeating)),
             ));
+        }
+    }
+}
+
+fn water_animation(
+    time: Res<Time>,
+    mut query: Query<(
+        &WaterAnimationIndices,
+        &mut WaterAnimationTimer,
+        &mut Sprite,
+    )>,
+) {
+    for (indices, mut timer, mut sprite) in &mut query {
+        timer.tick(time.delta());
+
+        if timer.just_finished() {
+            if let Some(atlas) = &mut sprite.texture_atlas {
+                atlas.index = if atlas.index == indices.last {
+                    indices.first
+                } else {
+                    atlas.index + 1
+                };
+            }
         }
     }
 }
