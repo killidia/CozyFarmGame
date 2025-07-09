de952474c9c1a26c44505c3928af116be2b4ae08
Dylan Adam <dylan41ad@hotmail.fr>
2025-07-09 19:09:46 +0200

    remove useless assets and clear layer in tiled editor

--- src/map.rs
+++ src/map.rs
@@ -4,10 +4,6 @@ pub struct MapPlugin;
 
 impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, load_map);
+        app;
     }
-}
-
-fn load_map(mut commands: Commands, asset_server: Res<AssetServer>) {
-    todo!()
 }
