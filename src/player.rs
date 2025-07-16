59138c420788160521d25c099d2a244b95985e68
Dylan Adam <dylan41ad@hotmail.fr>
2025-07-16 15:22:22 +0200

    player movement

--- src/player.rs
+++ src/player.rs
@@ -1,10 +1,15 @@
 use bevy::prelude::*;
 
+const PLAYER_SPEED: f32 = 50.0;
+
+#[derive(Component)]
+struct Player;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_player);
+        app.add_systems(Startup, setup_player).add_systems(Update, movement);
     }
 }
 
@@ -22,6 +27,37 @@ fn setup_player(mut commands: Commands, asset_server: Res<AssetServer>, mut text
             }
         ),
         Transform::from_xyz(32.0, -32.0, 3.0),
+        Player,
         Name::new("player"),
     ));
+}
+
+fn movement(keyboard_input: Res<ButtonInput<KeyCode>>, mut player_transform: Single<&mut Transform, With<Player>>, time: Res<Time>) {
+    let mut direction = Vec3::ZERO;
+
+    // horizontal movement
+    if keyboard_input.pressed(KeyCode::KeyD) {
+        direction.x += 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyA) {
+        direction.x -= 1.0;
+    }
+
+    // vertical movement
+    if keyboard_input.pressed(KeyCode::KeyW) {
+        direction.y += 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        direction.y -= 1.0;
+    }
+
+    // Normalize direction
+    if direction != Vec3::ZERO {
+        direction = direction.normalize();
+    }
+
+    // apply player movement
+    player_transform.translation += direction * PLAYER_SPEED * time.delta_secs();
 }
