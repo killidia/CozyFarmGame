88f78b0af031174796ced4aad295b43c76da6894
Dylan Adam <dylan41ad@hotmail.fr>
2025-07-31 21:05:08 +0200

    WIP: UI slicer

--- src/ui.rs
+++ src/ui.rs
@@ -8,10 +8,24 @@ impl Plugin for UiPlugin {
     }
 }
 
-fn spawn_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let ui = asset_server.load("ui/ui.png");
+fn spawn_ui(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load("ui/ui.png");
+    let atlas_layout =
+        TextureAtlasLayout::from_grid(UVec2::splat(48), 4, 4, None, Some(UVec2::splat(8)));
+    let atlas_layout_handle = texture_atlases.add(atlas_layout);
 
-    // TODO: splice image to display UI
+    // index: 7
+
+    let slicer = TextureSlicer {
+        border: BorderRect::all(8.0),
+        center_scale_mode: SliceScaleMode::Stretch,
+        sides_scale_mode: SliceScaleMode::Stretch,
+        max_corner_scale: 1.0,
+    };
 
     commands
         .spawn(Node {
@@ -32,11 +46,21 @@ fn spawn_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
                 ..default()
             });
 
-            builder.spawn(Node {
-                display: Display::Flex,
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                ..default()
-            });
+            builder
+                .spawn(Node {
+                    display: Display::Flex,
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..default()
+                })
+                .with_children(|builder| {
+                    builder.spawn((ImageNode::from_atlas_image(
+                        texture,
+                        TextureAtlas {
+                            index: 7,
+                            layout: atlas_layout_handle,
+                        },
+                    ),));
+                });
         });
 }
